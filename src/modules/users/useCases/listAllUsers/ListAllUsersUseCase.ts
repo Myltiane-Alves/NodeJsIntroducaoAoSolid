971ba956e5d6273642d9cb854289591979f8dd3f
Myltiane Alves <myltiane.aux@gmail.com>
2021-09-30 14:58:10 -0300

    Subindo arquivos de listagem de usuários

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,15 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
   user_id: string;
 }
-
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userIsAdmin = this.usersRepository.findById((user_id));
+
+    if(!userIsAdmin?.admin || !userIsAdmin) {
+      throw new Error("Mensagem de erro");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
